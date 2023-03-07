f63ddeb0f93ea44675ad342baa66e0f87f2ec1e2
osawa-koki <osawa-koki@example.com>
2023-03-07 11:41:26 +0900

    GetPublicKey_ReturnsExpectedValue

---
+++ Tests/GetPublicKey.test.cs
@@ -0,0 +1,17 @@
+﻿namespace my_simple_rsa
+{
+  public static partial class Test
+  {
+    [Theory]
+    [InlineData(3, 5, new[] { 15, 65537 })]
+    [InlineData(11, 17, new[] { 187, 65537 })]
+    [InlineData(101, 103, new[] { 10403, 65537 })]
+    [InlineData(631, 641, new[] { 404471, 65537 })]
+    [InlineData(10007, 10009, new[] { 100160063, 65537 })]
+    public static void GetPublicKey_ReturnsExpectedValue(int p, int q, int[] expectedOutput)
+    {
+      var expected = (expectedOutput[0], expectedOutput[1]);
+      Assert.Equal(expected, Util.GetPublicKey(p, q));
+    }
+  }
+}
