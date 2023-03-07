5afb44366356ab3743c13469693c7593126b9fd2
osawa-koki <osawa-koki@example.com>
2023-03-07 14:15:28 +0900

    小さな修正。

--- src/Util.Encrypt.cs
+++ src/Util.Encrypt.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Numerics;
-using System.Text;
+﻿using System.Numerics;
 
 namespace my_simple_rsa
 {
@@ -9,26 +6,21 @@ namespace my_simple_rsa
   {
     public static string Encrypt((BigInteger n, BigInteger e) publicKey, string message)
     {
-      var _message = Uri.EscapeDataString(message);
+      var _message = System.Web.HttpUtility.UrlEncode(message);
       var (n, e) = publicKey;
-      var blockSize = (int)Math.Floor(BigInteger.Log10(n) / Math.Log10(2)) - 1;
-      var bytes = Encoding.UTF8.GetBytes(_message);
-      var blocks = Enumerable.Range(0, bytes.Length / blockSize + 1)
-          .Select(i => bytes.Skip(i * blockSize).Take(Math.Min(blockSize, bytes.Length - i * blockSize)))
-          .Where(s => s.Any())
-          .Select(block => {
-            var padded = new byte[blockSize];
-            block.ToArray().CopyTo(new Span<byte>(padded));
-            return new BigInteger(padded);
-          })
-          .Select(num => num.ToString().PadLeft(blockSize, '0'))
-          .ToList();
-
+      var blockSize = (int)(BigInteger.Log10(n) / BigInteger.Log10(2)) - 1;
+      var blocks = _message.ToCharArray().Aggregate(new List<string>(), (acc, _char) =>
+      {
+        var code = (int)_char;
+        var padded = code.ToString().PadLeft(blockSize, '0');
+        acc.Add(padded);
+        return acc;
+      });
       var encryptedBlocks = blocks.Select(block =>
       {
         var num = BigInteger.Parse(block);
-        return BigInteger.ModPow(num, e, n).ToString().PadLeft(blockSize + 1, '0');
-      }).ToList();
+        return ModExp(num, e, n).ToString().PadLeft(blockSize + 1, '0');
+      });
       return string.Join("", encryptedBlocks);
     }
   }
